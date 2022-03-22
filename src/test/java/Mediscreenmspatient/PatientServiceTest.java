f3e4efcbb1685f15b62ca36628d8543f595338b2
mike_royer <contactdlicdl@gmail.com>
2022-03-22 16:55:55 +0100

    Ajout tests

--- src/test/java/Mediscreenmspatient/PatientServiceTest.java
+++ src/test/java/Mediscreenmspatient/PatientServiceTest.java
@@ -19,7 +19,7 @@ class PatientServiceTest {
     @Autowired
     PatientService patientService;
     LocalDate localDate = LocalDate.of(1983, 05, 17);
-    private final Patient patient = new Patient(null, "Mike", "ROYER", localDate, "Enorme", "12 bd de l improvisation", "11284212");
+    private final Patient patient = new Patient(null, "Mike", "ROYER", localDate, "Enorme", "12 bd de l'improvisation", "11284212");
 
     @Test
     void contextLoads() {
@@ -34,9 +34,31 @@ class PatientServiceTest {
     }
 
     @Test
-    
+    public void findPatientByIdTest() {
+        Patient patientCreated = patientService.createPatient(patient);
+        Patient patientSearched = patientService.findPatientById(patientCreated.getId());
+        Assertions.assertEquals("11284212", patientSearched.getPhone());
+        patientService.deletePatient(patientCreated.getId());
+    }
+
+    @Test
+    public void updatePatientTest() {
+        Patient patientCreated = patientService.createPatient(patient);
+        patientCreated.setPhone("12294313");
+        Patient patientUpdated = patientService.updatePatient(patientCreated, patientCreated.getId());
+        Assertions.assertEquals(patientUpdated.getPhone(), "12294313");
+        patientService.deletePatient(patientCreated.getId());
+    }
+
+    @Test
+    void deletePatientTest() {
 
 
+        Patient patientCreated = patientService.createPatient(patient);
 
+        patientService.deletePatient(patientCreated.getId());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> patientService.findPatientById(patientCreated.getId()));
+    }
 
 }
